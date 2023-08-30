8d18c52c1768651a4fa9c4f5b26543fab674df35
daniel marques <daniel.marquespinto@therealreal.com>
2023-08-30 20:18:20 -0300

    WIP

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,18 @@
-import '@/styles/globals.css'
+import React from "react";
+import Bugsnag from "@bugsnag/js";
+import BugsnagPluginReact from "@bugsnag/plugin-react";
+
+export const client = Bugsnag.start({
+  apiKey: process.env.NEXT_PUBLIC_BUGSNAG_API_KEY,
+  plugins: [new BugsnagPluginReact()],
+});
+
+const ErrorBoundary = Bugsnag.getPlugin("react").createErrorBoundary(React);
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ErrorBoundary>
+      <Component {...pageProps} />
+    </ErrorBoundary>
+  );
 }
